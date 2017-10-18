194cc6343cc23bbc1826764c3438745e100d667b
Marcio Lucas <marciioluucas@gmail.com>
2017-10-18 20:50:55 -0200

    Add project files.

---
+++ HelloWorldWF/Operacao.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldWF {
+    class Operacao {
+
+        int soma(int a, int b)
+        { 
+            return 0;
+        }
+    }
+}
---
+++ HelloWorldWFTest/OperacaoTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HelloWorldWFTest
+{
+    [TestClass]
+    public class OperacaoTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
